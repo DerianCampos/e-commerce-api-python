7231c896131e1adb3dc6db12d26bfc6709d3a154
deriancampos <71363391+DerianCampos@users.noreply.github.com>
2026-07-12 22:57:44 -0600

    feat: adding user composition layer

---
+++ src/app/composition/features/users.py
@@ -0,0 +1,34 @@
+from fastapi import Depends
+
+from src.app.composition.repositories import get_user_repository
+
+from app.features.user.application.use_cases.create_user import CreateUserUseCase
+from app.features.user.application.use_cases.delete_user import DeleteUserUseCase
+from app.features.user.application.use_cases.get_user_by_id import GetUserByIdUseCase
+from app.features.user.application.use_cases.update_user import UpdateUserUseCase
+
+from app.features.user.infrastructure.repository.user_repository_impl import UserRepositoryImpl
+
+
+async def get_create_user_use_case(
+        repo: UserRepositoryImpl = Depends(get_user_repository)
+        ) -> CreateUserUseCase:
+    return CreateUserUseCase(repo)
+
+
+async def get_get_user_by_id_use_case(
+        repo: UserRepositoryImpl = Depends(get_user_repository)
+        ) -> GetUserByIdUseCase:
+    return GetUserByIdUseCase(repo)
+
+
+async def get_update_user_use_case(
+        repo: UserRepositoryImpl = Depends(get_user_repository)
+        ) -> UpdateUserUseCase:
+    return UpdateUserUseCase(repo)
+
+
+async def get_delete_user_use_case(
+        repo: UserRepositoryImpl = Depends(get_user_repository)
+        ) -> DeleteUserUseCase:
+    return DeleteUserUseCase(repo)
