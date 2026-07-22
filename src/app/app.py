d573f47d75b04a200c3345fc87428ea15f0289da
deriancampos <71363391+DerianCampos@users.noreply.github.com>
2026-07-21 22:25:13 -0600

    feat: adding router features

--- src/app/app.py
+++ src/app/app.py
@@ -4,9 +4,11 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from src.app.config.app_config import AppConfig
-
+from src.app.features.band.presentation.band_routes import router as band_router
+from src.app.features.product.presentation.product_routes import router as product_router
+from src.app.features.product_variant.presentation.product_variant_routes import router as product_variant_router
+from src.app.features.t_shirt_size.presentation.t_shirt_size_routes import router as t_shirt_size_router
 from src.app.features.user.presentation.user_routes import router as user_router
-
 
 ENV = os.getenv("APP_ENV", "local")
 
@@ -22,17 +24,16 @@ if ENV not in ("local", "container"):
     fastapi_app.openapi_url = None
 
 # --- CORS Origins from config ---
-origins = [
-    "http://localhost:8080"
-]
+origins = ["http://localhost:8080"]
 
 fastapi_app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],  # Allows all methods
-    allow_headers=["*"],  # Allows all headers
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
+
 
 @fastapi_app.get("/")
 def read_root():
@@ -43,4 +44,9 @@ def read_root():
 def get_health_check():
     return "Ok"
 
+
 fastapi_app.include_router(user_router, prefix="/api/v1/user", tags=["User"])
+fastapi_app.include_router(band_router, prefix="/api/v1/bands", tags=["Band"])
+fastapi_app.include_router(product_router, prefix="/api/v1/products", tags=["Product"])
+fastapi_app.include_router(t_shirt_size_router, prefix="/api/v1/sizes", tags=["T-Shirt Size"])
+fastapi_app.include_router(product_variant_router, prefix="/api/v1/variants", tags=["Product Variant"])
